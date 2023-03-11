4fb78f12913bceeaf4a28f33f2a34dbb23fe1815
accountisgod1 <123174826+accountisgod1@users.noreply.github.com>
2023-03-11 20:56:37 +0800

    Updating Game.py
    Adding More Levels

--- Game.py
+++ Game.py
@@ -7,16 +7,34 @@ import Enemies as En
 import PlayerStats as P
 import random
 
-#Main
 
+#Un-used variables
+EquippedSword = {}
+EquippedSpear = {}
+EquippedShield = {}
+
+#Main
 
 SwordEquipped = False
 SpearEquipped = False
 ShieldEquipped = False
 
-EquippedSword = {}
-EquippedSpear = {}
-EquippedShield = {}
+def check_stamina():
+    while P.Stamina < P.MaxStamina:
+        P.Stamina += 1
+
+def check_health():
+    while P.Health < P.MaxHealth:
+        P.Health += 1
+
+def XP():
+   if P.xp == 100:
+    P.Level += 1
+    print("Level UP! Level: ",P.Level)
+
+   elif P.xp >= 200:
+     P.Level += 2
+     print("Level UP! Level: ",P.Level)
 
 os.makedirs("Achivements",exist_ok=True)
 
@@ -41,24 +59,14 @@ if TutorialInput.lower() == "a":
         print(f"Stamina: {P.Stamina}")
         En.EnemiesStats["Goblin"]["Health"] -=10
         print("Health:",En.EnemiesStats["Goblin"]["Health"])
+        check_stamina()
         if En.EnemiesStats["Goblin"]["Health"] == 0:
            print("YOU WON!!")
            xp = 100
            print(xp)
            P.Coins += 100
            print("Coins:",P.Coins)
-
-while P.Stamina < P.MaxStamina:
-    P.Stamina += 1
-
-#XP BLOCKS
-if P.xp == 100:
-    P.Level += 1
-    print("Level UP! Level: ",P.Level)
-
-elif P.xp >= 200:
-    P.Level += 2
-    print("Level UP! Level: ",P.Level)
+           En.EnemiesStats["Goblin"]["Health"] = 10
 
 
 TutorialInput = "Wow! You Defeated A Goblin!"
@@ -164,10 +172,11 @@ if TutorialInput.lower() == "v":
         TutorialInput = input("S To Increase Stamina ")
         if TutorialInput.lower() == "s":
             P.MaxStamina += 35
+            check_stamina()
             print("Your Stamina Is: ",P.MaxStamina)
 
 
-SelectionInput = input("V To Go To The Village, A To Attack")
+SelectionInput = input("V To Go To The Village, A To Attack ")
 
 if SelectionInput.lower() == "a":
    print("You Are Going To Fight A Skeleton Warrior")
@@ -182,14 +191,17 @@ if SelectionInput.lower() == "a":
        print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
        En.EnemiesStats["Skeleton Warrior"]["Health"] -= 10
        print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
-       if En.EnemiesStats["Skeleton Warrior"]["Health"] == 0:
+       if En.EnemiesStats["Skeleton Warrior"]["Health"] <= 0:
           print("YOU WON!!")
           xp = 100
           print(xp)
           P.Coins += 100
           print("Coins:",P.Coins)
+          print("A random traveller asked you to take a dangerous journey. You accepted")
+          En.EnemiesStats["Skeleton Warrior"]["Health"] = 20
+          check_stamina()
 
-   elif AttackInput == "Sword":
+   elif AttackInput.lower() == "sword":
       if SwordEquipped == True:
           if InStock0 == True:
              P.Stamina -= StaminaUsage
@@ -199,12 +211,15 @@ if SelectionInput.lower() == "a":
              print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
              En.EnemiesStats["Skeleton Warrior"]["Health"] -= DamageAmount
              print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
-             if En.EnemiesStats["Skeleton Warrior"]["Health"] == 0:
+             if En.EnemiesStats["Skeleton Warrior"]["Health"] <= 0:
                 print("YOU WON!!")
                 xp = 100
                 print(xp)
                 P.Coins += 100
                 print("Coins:",P.Coins)
+                print("A random traveller asked you to take a dangerous journey. You accepted")
+                En.EnemiesStats["Skeleton Warrior"]["Health"] = 20
+                check_stamina()
 
           elif InStock1 == True:
              P.Stamina -= StaminaUsage1
@@ -214,12 +229,16 @@ if SelectionInput.lower() == "a":
              print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
              En.EnemiesStats["Skeleton Warrior"]["Health"] -= DamageAmount
              print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
-             if En.EnemiesStats["Skeleton Warrior"]["Health"] == 0:
+             if En.EnemiesStats["Skeleton Warrior"]["Health"] <= 0:
                 print("YOU WON!!")
                 xp = 100
                 print(xp)
                 P.Coins += 100
                 print("Coins:",P.Coins)
+                print("A random traveller asked you to take a dangerous journey. You accepted")
+                En.EnemiesStats["Skeleton Warrior"]["Health"] = 20
+                check_stamina()
+
 
 elif SelectionInput.lower() == "v":
     print("Loading Village")
@@ -246,9 +265,6 @@ elif SelectionInput.lower() == "v":
 
            if SwordName == SwordName1:
                SwordName1 = random.choice(SwordsList)
-
-           if SwordName != "WoodenSword":
-              SwordName = "Wooden Sword"
 
            InStock = [SwordName,SwordName1]
 
@@ -298,8 +314,199 @@ elif SelectionInput.lower() == "v":
 
                    
 
-    elif TutorialInput.lower() == "t":
-        TutorialInput = input("S To Increase Stamina ")
-        if TutorialInput.lower() == "s":
+    elif VillageInput.lower() == "t":
+        TrainingInput = input("S To Increase Stamina, H To Increase Health")
+        if TrainingInput.lower() == "s":
             P.MaxStamina += 35
+            check_stamina()
             print("Your Stamina Is: ",P.MaxStamina)
+
+        elif TrainingInput.lower() == "h":
+            P.MaxHealth += 35
+            check_health()
+            print("Your Health Is: ",P.MaxHealth)
+
+
+
+
+#Dungeon
+
+print("You arrived, you found out the place is actually a dungeon.")
+
+SelectionInput = input("V To Go To The Village, A To Attack ")
+
+if SelectionInput.lower() == "a":
+   print("You Are Going To Fight A Skeleton")
+   print("Health: ",En.EnemiesStats["Skeleton"]["Health"])
+   print("Damage: ",En.EnemiesStats["Skeleton"]["Damage"])
+   AttackInput = input("What Do You Want To Attack With: ")
+   if AttackInput == "Punch":
+       P.Stamina - S.Punch_Stamina
+       print(f"Stamina: {P.Stamina}")
+       En.EnemiesStats["Skeleton"]["Health"] -= 10
+       En.EnemiesStats["Skeleton"]["Health"] += 0.5
+       print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+       En.EnemiesStats["Skeleton"]["Health"] -= 10
+       print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+       if En.EnemiesStats["Skeleton"]["Health"] <= 0:
+          print("YOU WON!!")
+          xp = 100
+          print(xp)
+          P.Coins += 100
+          print("Coins:",P.Coins)
+          En.EnemiesStats["Skeleton"]["Health"] = 20
+          check_stamina()
+
+   elif AttackInput.lower() == "sword":
+      if SwordEquipped == True:
+          if InStock0 == True:
+             P.Stamina -= StaminaUsage
+             print(f"Stamina: {P.Stamina}")
+             En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount
+             En.EnemiesStats["Skeleton"]["Health"] += 0.5
+             print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+             En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount
+             print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+             if En.EnemiesStats["Skeleton"]["Health"] <= 0:
+                print("YOU WON!!")
+                xp = 100
+                print(xp)
+                P.Coins += 100
+                print("Coins:",P.Coins)
+                En.EnemiesStats["Skeleton"]["Health"] = 20
+                check_stamina()
+
+          elif InStock1 == True:
+             P.Stamina -= StaminaUsage1
+             print(f"Stamina: {P.Stamina}")
+             En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount1
+             En.EnemiesStats["Skeleton"]["Health"] += 0.5
+             print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+             En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount
+             print("Health:",En.EnemiesStats["Skeleton"]["Health"])
+             if En.EnemiesStats["Skeleton"]["Health"] <= 0:
+                print("You Are Fighting 2 Goblins.")
+                print("Health: ",En.EnemiesStats["Goblin"]["Health"]+10)
+                print("Damage: ",En.EnemiesStats["Goblin"]["Damage"]+10)
+                AttackInput = input("What Do You Want To Attack With: ")
+                if AttackInput == "Punch":
+                    P.Stamina - S.Punch_Stamina
+                    print(f"Stamina: {P.Stamina}")
+                    En.EnemiesStats["Goblin"]["Health"] -= 10
+                    print("Health:",En.EnemiesStats["Goblin"]["Health"])
+                    En.EnemiesStats["Goblin"]["Health"] -= 10
+                    print("Health:",En.EnemiesStats["Goblin"]["Health"])
+                    if En.EnemiesStats["Goblin"]["Health"] <= 0:
+                        print("YOU WON!!")
+                        xp = 100 
+                        print(xp)
+                        P.Coins += 100
+                        print("Coins:",P.Coins)
+                        En.EnemiesStats["Goblin"]["Health"] = 10
+                        check_stamina()
+
+                elif AttackInput.lower() == "sword":
+                    if SwordEquipped == True:
+                       if InStock0 == True:
+                          P.Stamina -= StaminaUsage
+                          print(f"Stamina: {P.Stamina}")
+                          En.EnemiesStats["Goblin"]["Health"] -= DamageAmount
+                          En.EnemiesStats["Goblin"]["Health"] += 0.5
+                          print("Health:",En.EnemiesStats["Goblin"]["Health"])
+                          En.EnemiesStats["Goblin"]["Health"] -= DamageAmount
+                          print("Health:",En.EnemiesStats["Goblin"]["Health"])
+                          if En.EnemiesStats["Goblin"]["Health"] <= 0:
+                             print("YOU WON!!")
+                             xp = 100
+                             print(xp)
+                             P.Coins += 100
+                             print("Coins:",P.Coins)
+                             En.EnemiesStats["Skeleton Warrior"]["Health"] = 20
+                             check_stamina()
+
+                       elif InStock1 == True:
+                            P.Stamina -= StaminaUsage1
+                            print(f"Stamina: {P.Stamina}")
+                            En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount1
+                            En.EnemiesStats["Skeleton"]["Health"] += 0.5
+                            print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
+                            En.EnemiesStats["Skeleton"]["Health"] -= DamageAmount
+                            print("Health:",En.EnemiesStats["Skeleton Warrior"]["Health"])
+                            print("YOU WON!!")
+                            xp = 100
+                            print(xp)
+                            P.Coins += 100
+                            print("Coins:",P.Coins)
+                            En.EnemiesStats["Skeleton Warrior"]["Health"] = 20
+                            check_stamina()
+
+
+elif SelectionInput.lower() == "v":
+    print("Loading Village")
+    VillageInput = input("W To Go The Weapon Shop, T For Training ")
+
+    if VillageInput.lower() == "w":
+       print("This Place Has Every Sword, Spear, Shield In The World!")
+       WeaponInput = input("S For Sword, SP For Spear, SH For Shield ")
+       if WeaponInput.lower() == "s":
+           SwordsList = ["Iron Sword","Diamond Sword","Wooden Sword","Ancient Sword","Gold Sword"]
+           SpecialSwordList = ["Excalibur","Unknown Sword"]
+
+           SwordsStats = {"Excalibur": {"Damage": 2500, "StaminaUsage": 90,"Price": 10000}, 
+                    "Iron Sword": {"Damage": 50, "StaminaUsage": 10, "Price": 254},
+                    "Unknown Sword": {"Damage": 500, "StaminaUsage": 67,"Price": 4700},
+                    "Diamond Sword": {"Damage": 200, "StaminaUsage": 20,"Price": 1420},
+                    "Wooden Sword": {"Damage": 21, "StaminaUsage": 6,"Price": 100},
+                    "Ancient Sword": {"Damage": 1075,"StaminaUsage": 56,"Price": 4203},
+                    "Gold Sword": {"Damage": 142, "StaminaUsage": 17, "Price": 1204}}
+
+           SwordName = random.choice(SwordsList)
+
+           SwordName1 = random.choice(SwordsList)
+
+           if SwordName == SwordName1:
+               SwordName1 = random.choice(SwordsList)
+
+           InStock = [SwordName,SwordName1]
+
+           DamageAmount = SwordsStats.get(SwordName).get("Damage")
+
+           StaminaUsage = SwordsStats.get(SwordName).get("StaminaUsage")
+
+           Price = SwordsStats.get(SwordName).get("Price")
+
+           DamageAmount1 = SwordsStats.get(SwordName1).get("Damage")
+
+           StaminaUsage1 = SwordsStats.get(SwordName1).get("StaminaUsage")
+
+           Price1 = SwordsStats.get(SwordName1).get("Price")
+
+           print(f"Swords In Stock: {InStock[0]}, {InStock[1]}")
+           
+           BuyInput = input("Pick Your Swords In Stock. ")
+
+           if BuyInput == InStock[0]:
+               print("You Are About To Buy: ",InStock[0])
+               print(f"This Is The Stats, Damage: {DamageAmount} Stamina Usage:{StaminaUsage} Price: {Price} Coins")
+               BuyInputCon = input("Do You Want To By This Sword? (Y/n) ")
+               if BuyInputCon.lower() == "y":
+                if P.Coins == Price:
+                   InStock0 = True
+                   SwordEquipped = True
+                   EquippedSword = InStock[0]
+
+                elif P.Coins < Price:
+                   print("You Don't Have Enough Coins")
+
+           elif BuyInput == InStock[1]:
+                print("You Are About To Buy: ",InStock[1])
+                print(f"This Is The Stats, Damage: {DamageAmount1} ,Stamina Usage: {StaminaUsage1}, Price: {Price1} Coins")
+                BuyInputCon = input("Do You Want To By This Sword? (Y/n) ")
+                if BuyInputCon.lower() == "y":
+                 if P.Coins == Price:
+                   InStock1 = True
+                   SwordEquipped = True
+                   EquippedSword = InStock[1]
+
+                 elif P.Coins < Price:
+                    print("You Don't Have Enough Coins")
